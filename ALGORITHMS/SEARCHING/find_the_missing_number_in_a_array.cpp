51707d95ec38a494275e7d6b54f8d60e8f3ef29b
Uditkumar13 <Uditkumar2401@gmail.com>
2018-10-17 02:32:41 +0530

    finding a missing  ele in a sorted array

--- ALGORITHMS/SEARCHING/find_the_missing_number_in_a_array.cpp
+++ ALGORITHMS/SEARCHING/find_the_missing_number_in_a_array.cpp
@@ -7,9 +7,9 @@ int l=0, r=N-1;
 
     int mid=(l+r)/2;
 // it will have the position for the missing element
-    if (arr[mid]!=mid+1 && arr[mid-1]==mid)
+    if (arr[mid]!=mid+1 && arr[mid-1]==mid){
     cout<< "Missing number is : ";
-    return mid+1;
+    return mid+1;}
 
     if (arr[mid]!=mid+1)
     r=mid-1;
@@ -19,12 +19,12 @@ int l=0, r=N-1;
 
   }
   //if no element is missing return -1
-  return -1
+  return -1;
 }
 
 int main(){
   int arr[]={1,2,3,4,5,6,7,8,9,10,11,12,14};
   int N=sizeof(arr)/sizeof(arr[0]);
-  cout<<find_ele(arr,N);
+  cout<<finding_ele(arr,N);
   return 0;
 }
