b5363341080c3d0e54849ae0103c214a82b9e809
Uditkumar13 <Uditkumar2401@gmail.com>
2018-10-16 23:47:59 +0530

    finding missing ele in sorted array using binary approach

--- ALGORITHMS/SEARCHING/find_the_missing_number_in_a_array.cpp
+++ ALGORITHMS/SEARCHING/find_the_missing_number_in_a_array.cpp
@@ -5,7 +5,18 @@ int finding_ele(int arr[], int N){
 int l=0, r=N-1;
   while (l<=r){
 
-    int mid=l+r/2;
-    
+    int mid=(l+r)/2;
+
+    if (arr[mid]!=mid+1 && arr[mid-1]==mid)
+    return mid+1;
+
+    if (arr[mid]!=mid+1)
+    r=mid-1;
+
+    else
+    l=mid+1;
+
   }
+  //if no element is missing return -1
+  return -1
 }
