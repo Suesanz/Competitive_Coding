070bb28fae36d059288804d69052b4445cbdfe81
Uditkumar13 <Uditkumar2401@gmail.com>
2018-10-17 23:47:08 +0530

    diamond pattern

--- BASIC_PROGRAMMING/OPERATORS/diamond_pattern.cpp
+++ BASIC_PROGRAMMING/OPERATORS/diamond_pattern.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 int main(){
 int N;
+int space=N-1;
 // no. of rows user want to see of the diamond pattern
+for (int i=1;i<=N;i++){
+for (int j=1;j<=space;j++){
+  cout<<" ";
+}
 
+space--;
+
+for (int j=1;j=2*i-1;j++){
+  cout<< "*";
+  cout<<" ";
+}
+  
+}
 return 0;
 }
