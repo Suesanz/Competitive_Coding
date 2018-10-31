ce16f83b5016009eee2312cd0dbb1a6df7152973
Uditkumar13 <Uditkumar2401@gmail.com>
2018-10-31 23:25:43 +0530

    first machine learning code

---
+++ MACHINE_LEARNING/convolutional_layer_behaviour.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as pyplot
+img1=np.array([np.array([200,200]),[np.array([200,200])])
+img2=np.array([np.array([200,200]),[np.array([0,0])])
+img3=np.array([np.array([200,0]),[np.array([200,0])])
