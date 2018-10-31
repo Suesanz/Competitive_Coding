27ce4a6ba62eeee4dd6db393117370dbfc691d2f
Uditkumar13 <Uditkumar2401@gmail.com>
2018-10-31 23:28:12 +0530

    first machine learning code

--- MACHINE_LEARNING/convolutional_layer_behaviour.py
+++ MACHINE_LEARNING/convolutional_layer_behaviour.py
@@ -3,3 +3,13 @@ import matplotlib.pyplot as pyplot
 img1=np.array([np.array([200,200]),[np.array([200,200])])
 img2=np.array([np.array([200,200]),[np.array([0,0])])
 img3=np.array([np.array([200,0]),[np.array([200,0])])
+kernel_horizontal = np.array([np.array([2, 2]), np.array([-2, -2])])
+print(kernel_horizontal, 'is a kernel for detecting horizontal edges')
+
+kernel_vertical = np.array([np.array([2, -2]), np.array([2, -2])])
+print(kernel_vertical, 'is a kernel for detecting vertical edges')
+# We will apply the kernels on the images by
+# elementwise multiplication followed by summation
+def apply_kernel(img, kernel):
+    return np.sum(np.multiply(img, kernel))
+    
