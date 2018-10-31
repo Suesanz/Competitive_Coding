1ed04491fa14676e74c7424874571e1de26a65c5
Uditkumar13 <Uditkumar2401@gmail.com>
2018-10-31 23:30:06 +0530

    convolutional layer multiplication

--- MACHINE_LEARNING/convolutional_layer_behaviour.py
+++ MACHINE_LEARNING/convolutional_layer_behaviour.py
@@ -12,4 +12,39 @@ print(kernel_vertical, 'is a kernel for detecting vertical edges')
 # elementwise multiplication followed by summation
 def apply_kernel(img, kernel):
     return np.sum(np.multiply(img, kernel))
-    
+
+
+# Visualizing img1
+plt.imshow(img1)
+plt.axis('off')
+plt.title('img1')
+plt.show()
+# Checking for horizontal and vertical features in image1
+print('Horizontal edge confidence score:', apply_kernel(img1,
+                                            kernel_horizontal))
+print('Vertical edge confidence score:', apply_kernel(img1,
+                                            kernel_vertical))
+
+# Visualizing img2
+plt.imshow(img2)
+plt.axis('off')
+plt.title('img2')
+plt.show()
+
+# Checking for horizontal and vertical features in image2
+print('Horizontal edge confidence score:', apply_kernel(img2,
+                                            kernel_horizontal))
+print('Vertical edge confidence score:', apply_kernel(img2,
+                                            kernel_vertical))
+
+# Visualizing img3
+plt.imshow(img3)
+plt.axis('off')
+plt.title('img3')
+plt.show()
+
+# Checking for horizontal and vertical features in image3
+print('Horizontal edge confidence score:', apply_kernel(img3,
+                                            kernel_horizontal))
+print('Vertical edge confidence score:', apply_kernel(img3,
+                                            kernel_vertical)) 
