3a7d8196a04458b794e1b6918dca94050d10a304
Uditkumar13 <Uditkumar2401@gmail.com>
2018-11-18 19:12:50 +0530

    finding_subset

--- ALGORITHMS/DYNAMIC-PROGRAMMING/finding_subset.cpp
+++ ALGORITHMS/DYNAMIC-PROGRAMMING/finding_subset.cpp
@@ -1,4 +1,7 @@
 #include<bits/stdc++.h>
+#include<math.h>
+#include<string>
+
 using namespace std;
 
 //utility function to split the string using a delim .
@@ -11,5 +14,25 @@ while (getline(ss,item,delim))
 return elems;
 
 }
+
+// function to find  all the  subset of a set 
+int print_powerset(int arr[],int n){
+vector<string>list;
+// for loop very important 
+for (int i=0;i<int pow(2,n);i++){
+string subset ="" ;
+// consider the all element in the set 
+for (int j=0;j<n;j++){
+// Check if jth bit in the i is set. If the bit 
+            // is set, we consider jth element from set 
+if ((i & (1<<j))!=0)
+subset+=to_string(arr[j]) + "|";}
+  // if subset is encountered for the first time 
+        // If we use set<string>, we can directly insert 
+   if (find(list.begin(), list.end(), subset) == list.end()) 
+            list.push_back(subset); 
+    } 
+  
+
 
 
