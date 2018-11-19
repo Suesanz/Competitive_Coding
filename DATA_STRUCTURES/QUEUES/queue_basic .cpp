c922f94c36afde53cec8b390028cba1bb26f2244
Udit Kumar <uditkumar2401@gmail.com>
2018-11-19 23:49:10 +0530

    Add files via upload
    queue modified

--- DATA_STRUCTURES/QUEUES/queue_basic .cpp
+++ DATA_STRUCTURES/QUEUES/queue_basic .cpp
@@ -9,3 +9,16 @@ int front ,rear,size;
 unsigned capacity;
 int* array;
 };
+//function to create a queue of given capacity 
+// it initializes size of queue as 0 
+struct Queue* createQueue(unsigned capacity)
+{
+struct Queue* queue=(struct Queue*)malloc(sizeof(struct Queue));
+queue->capacity=capacity;
+queue->front =queue->size=0;
+queue->rear= capacity-1;
+queue->array=(int*)malloc(queue->capacity*sizeof(int));
+return queue;
+}
+
+//Queue is full when size become
