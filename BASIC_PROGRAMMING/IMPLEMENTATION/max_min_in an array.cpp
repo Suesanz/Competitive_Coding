bbd919b8049562985b285b5e2dd9ef9f6c85c97a
Udit Kumar <uditkumar2401@gmail.com>
2018-11-20 23:59:45 +0530

    optimaize

--- BASIC_PROGRAMMING/IMPLEMENTATION/max_min_in an array.cpp
+++ BASIC_PROGRAMMING/IMPLEMENTATION/max_min_in an array.cpp
@@ -13,3 +13,15 @@ minmax.max=arr[0];
 minmax.min=arr[0];
 return minmax;
 }
+// if there are more ele in an array , then initialize min and max
+if (arr[0]>arr[1])
+{minmax.max=arr[0];
+minmax.min=arr[1];
+}
+else{minmax.max=arr[1];
+minmax.min=arr[0];
+}
+for (i=2;i<n;i++){
+if (arr[i]>minmax.max)
+minmax.max=arr[i];
+else if (arr[i]<minmax.min)
