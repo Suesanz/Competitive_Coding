938fb3201305e1f9037378ba3028d67eb65fbf4a
Udit Kumar <uditkumar2401@gmail.com>
2018-11-21 00:03:58 +0530

    minimum or maximum in an arrray

---
+++ max_min_in an array.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<iostream>
+
+struct pair{
+int min;
+int max;
+};
+struct pair getMinMax(int arr[],int n){
+struct pair minmax;
+int i;
+// if there is only one element then return it as min max both
+if (n==1)
+{
+minmax.max=arr[0];
+minmax.min=arr[0];
+return minmax;
+}
+// if there are more ele in an array , then initialize min and max
+if (arr[0]>arr[1])
+{minmax.max=arr[0];
+minmax.min=arr[1];
+}
+else{minmax.max=arr[1];
+minmax.min=arr[0];
+}
+for (i=2;i<n;i++){
+if (arr[i]>minmax.max)
+minmax.max=arr[i];
+else if (arr[i]<minmax.min)
+minmax.min=arr[i];
+}
+return minmax;
+}
+
+//driver program to test above function 
+int main(){
+int arr[]={1000,11,445,1,330,3000};
+int arr_size=6;
+struct pair minmax= getMinMax(arr,arr_size);
+cout<<"nMinimum element is "<<minmax.min;
+cout<<"nMaximum element is "<<minmax.max;
+getchar();
+return 0;
+}
