7c4002ce8a71c630a907e4bfe0cebe0aebc3be45
Sourav Yadav <yadavsourav24071998@gmail.com>
2019-10-02 01:50:03 +0530

    Updated

--- DATA_STRUCTURES/LINKED_LIST/travering_singly_linked_list.cpp
+++ DATA_STRUCTURES/LINKED_LIST/travering_singly_linked_list.cpp
@@ -1,39 +1,70 @@
-#include<iostream>
-#include<stdlib.h>
-using namespace std;
-
-struct Node{
-  int data;
-struct Node *next;
-};
-
-// this function will print all the content of linked list
-void print_list(struct Node *n){
-
-  while (n!=NULL){
-    cout<<n->data<<" ";
-    n=n->next;
-  }
-}
-
-int main(){
-  struct Node* head=NULL;
-    struct Node* second =NULL;
-      struct Node* third=NULL;
-
-  //allocates 3 nodes in heap
-  head=(struct Node*)malloc(sizeof(struct Node));
-  second =(struct Node*)malloc(sizeof(struct Node));
-  third =(struct Node*)malloc(sizeof(struct Node));
-
-
-  head->data=1;// assign data to head of linked list
-  head->next=second ;// linking list to next node
-  second ->data=2;// assign data to second ele of linked list
-  second ->next=third ;
-  third ->data=3;// assign data to head of linked list
-  third ->next=NULL ;
-cout<< "Traversing through a single linked list \n";
-  print_list(head);
-  return 0;
-}
+#include<stdio.h>  
+#include<stdlib.h>  
+void create(int);  
+void traverse();  
+struct node  
+{  
+    int data;  
+    struct node *next;  
+};  
+struct node *head;  
+void main ()  
+{  
+    int choice,item;  
+    do   
+    {  
+        printf("\n1.Append List\n2.Traverse\n3.Exit\n4.Enter your choice?");  
+        scanf("%d",&choice);  
+        switch(choice)  
+        {  
+            case 1:  
+            printf("\nEnter the item\n");  
+            scanf("%d",&item);  
+            create(item);  
+            break;   
+            case 2:  
+            traverse();  
+            break;   
+            case 3:  
+            exit(0);  
+            break;    
+            default:  
+            printf("\nPlease enter valid choice\n");  
+        }  
+                  
+    }while(choice != 3);  
+}  
+void create(int item)  
+    {  
+        struct node *ptr = (struct node *)malloc(sizeof(struct node *));  
+        if(ptr == NULL)  
+        {  
+            printf("\nOVERFLOW\n");  
+        }  
+        else  
+        {  
+            ptr->data = item;  
+            ptr->next = head;  
+            head = ptr;  
+            printf("\nNode inserted\n");  
+        }  
+          
+    }  
+void traverse()  
+    {  
+        struct node *ptr;     
+        ptr = head;   
+        if(ptr == NULL)  
+        {  
+            printf("Empty list..");  
+        }  
+        else  
+        {  
+            printf("printing values . . . . .\n");   
+            while (ptr!=NULL)  
+            {  
+                printf("\n%d",ptr->data);  
+                ptr = ptr -> next;  
+            }  
+        }  
+     }  
